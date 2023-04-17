6495fb05ef20f3d89f38b51b4a9ec851a1118a7b
Efren Vasquez <efren.vasquez@wsu.edu>
2023-04-17 13:20:07 -0500

    Enhancement | Download fact sheet reports functionality

--- cahnrswsuwp-pods-roles.php
+++ cahnrswsuwp-pods-roles.php
@@ -3,7 +3,7 @@
  * Plugin Name: CAHNRS Pods Roles
  * Plugin URI: 
  * Description: This plugin changes some of the roles for the Pods plugin
- * Version: 1.1.3
+ * Version: 1.2.0
  * Author: Washington State University | CAHNRS Communications
  * Author URI: http://cahnrs.wsu.edu/communications
  */
@@ -60,3 +60,109 @@ add_action('save_post', 'update_fact_sheet_revision_date', 10, 1);
 
 // Hook the function to the 'before_delete_post' action to handle Pesticide deletions
 add_action('before_delete_post', 'update_fact_sheet_revision_date', 10, 1);
+
+require_once 'download-excel.php';
+
+add_action( 'admin_menu', 'create_custom_post_type_settings_page' );
+
+function create_custom_post_type_settings_page() {
+    add_submenu_page(
+        'edit.php?post_type=fact_sheet',
+        'Reports',
+        'Reports',
+        'manage_options',
+        'custom_post_type_settings',
+        'custom_post_type_settings_page'
+      );
+}
+
+function custom_post_type_settings_page() {
+    // Check if the user has permissions to access the settings page
+    if ( ! current_user_can( 'manage_options' ) ) {
+      wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+  
+    // Output the settings page HTML
+    echo '<div class="wrap">';
+
+    ?>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  <?php
+  // # build the URL and query string
+  $get_vars_excel = $_GET;
+  $get_vars_excel['excel'] = true;
+  $get_vars_excel['download_excel'] = true;
+  $excel_url = site_url() . '?' . http_build_query($get_vars_excel);
+  ?>
+
+
+
+	<?php
+  $today = getdate();
+    $args = array( 
+        'post_type' => 'fact_sheet',
+        //'nopaging' => true,
+        'posts_per_page' => -1,
+        'orderby' => 'modified',
+        'date_query'    => array(
+          'column'  => 'post_modified',
+          'after'   => '- 365 days'
+        )
+    );
+
+    $fact_sheet_query = new WP_Query($args);
+  ?> 
+  <h1>Fact Sheet Report</h1>
+  <p>Below are the most recent fact sheets that have been modified in the past 12 months. To download to an excel file, click on the "Download Report" button below.</p>
+  <div class="row" style="margin-top: 30px;">
+    <div class="col-sm-12">
+        <div class="dropdown">
+          <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background: #A60F2D;">
+            Download Report
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="<?php echo $excel_url ?>">Download as Excel</a>
+          </div>
+        </div>
+    </div>
+  </div>
+
+  <table>
+    <thead>
+      <th>Post</th>
+      <th>Revised Date</th>
+    </thead>
+  <?php 
+      if($fact_sheet_query->have_posts()){
+          while($fact_sheet_query->have_posts()){
+              $fact_sheet_query->the_post();
+              echo "<tr>";
+              echo "<td style='padding: 0.3rem 1.5rem .3rem .3rem;border: 1px solid #b3b3b3;border-collapse: collapse;'><a href='" . get_the_permalink() . "'>". get_the_title(). "</a></td>";
+              echo "<td style='padding: 0.3rem 1.5rem;border: 1px solid #b3b3b3;border-collapse: collapse;'>" . get_the_modified_date() . "</td>";
+              echo "</tr>";
+          }
+      } ?>
+  </table>
+
+  <div class="row" style="margin-top: 30px;">
+    <div class="col-sm-12">
+        <div class="dropdown">
+          <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background: #A60F2D;">
+            Download Report
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="<?php echo $excel_url ?>">Download as Excel</a>
+          </div>
+        </div>
+    </div>
+  </div> 
+  <?php
+
+
+    echo '</div>';
+  }
---
+++ download-excel.php
@@ -0,0 +1,98 @@
+<?php 
+class DOWNLOAD_EXCEL
+{
+  function __construct()
+  {
+    // I use the download feature on the frontend so I use the init action hook.
+    // If you use the download feature on the backend, you will use the admin_init action hook.
+    add_action('init', array($this, 'print_excel'));
+  }
+
+
+  function print_excel()
+  {
+
+    // # check the URL in order to perform the downloading
+    if (!isset($_GET['excel']) || !isset($_GET['download_excel'])) {
+      return false;
+    }
+
+
+    // # check the XLSXWriter class is already loaded or not. If it is not loaded yet, we will load it.
+    if (!class_exists('XLSXWriter')) {
+      include_once('inc/xlsxwriter.class.php');
+    }
+
+    $today = date('Y');
+   
+    // # set the destination file
+    $fileLocation = "$today Revised Factsheets.xlsx";
+
+    
+    $post_args = array( 
+        'post_type' => 'fact_sheet',
+        'posts_per_page' => -1,
+        'orderby' => 'modified',
+        'date_query'    => array(
+          'column'  => 'post_modified',
+          'after'   => '- 365 days'
+        )
+    );
+
+    $fact_sheet_query = new WP_Query($post_args);
+    
+    $header = array(
+      'Name'=>'string',
+      'Modified Date'=>'date',
+      'Link'=>'string',
+    );
+    
+    
+    
+    if($fact_sheet_query->have_posts()){
+      while($fact_sheet_query->have_posts()){
+          $fact_sheet_query->the_post();
+
+          
+          $title = get_the_title();
+          $modifiedDate = get_the_modified_date('Y-m-d');
+          
+          $postPermalink = get_permalink();
+
+          $data[] = array("$title", $modifiedDate, "$postPermalink");
+      }
+  } 
+
+    // # call the class and generate the excel file from the $data
+    $writer = new XLSXWriter();
+    $widths = array(40,20,50);
+    $col_options = array('widths'=>$widths);
+
+    $writer->writeSheetHeader('Sheet1', $header, $widths );
+    foreach($data as $row)
+	    $writer->writeSheetRow('Sheet1', $row  );
+    $writer->writeToFile($fileLocation);
+
+
+    // # prompt download popup
+    header('Content-Description: File Transfer');
+    header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    header("Content-Disposition: attachment; filename=" . basename($fileLocation));
+    header("Content-Transfer-Encoding: binary");
+    header("Expires: 0");
+    header("Pragma: public");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header('Content-Length: ' . filesize($fileLocation));
+
+    ob_clean();
+    flush();
+
+    readfile($fileLocation);
+    unlink($fileLocation);
+    exit;
+  }
+}
+
+// # initialize the class
+new DOWNLOAD_EXCEL();
+?>
